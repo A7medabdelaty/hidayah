78f2b758b5dde33c95ba044ce41ae907c0708d7b
A7medabdelaty <ahmed.abdelaty174@gmail.com>
2025-04-16 18:15:21 +0200

    add initial project setup and configuration files

---
+++ lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/constants/app_constants.dart';
+import 'core/cubit/theme/theme_cubit.dart';
+import 'core/routing/app_router.dart';
+import 'core/theme/app_theme.dart';
+
+class HidayahApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const HidayahApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, themeState) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: AppConstants.appName,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: themeState.themeMode,
+        locale: context.locale,
+        supportedLocales: context.supportedLocales,
+        localizationsDelegates: context.localizationDelegates,
+        onGenerateRoute: appRouter.generateRoute,
+        initialRoute: '/',
+      ),
+    );
+  }
+}
---
+++ lib/core/constants/strings_constants.dart
@@ -0,0 +1,9 @@
+class StringsConstants {
+  static const Map<String, String> en = {
+    'appName': 'Hidayah',
+  };
+
+  static const Map<String, String> ar = {
+    'appName': 'هداية',
+  };
+}
