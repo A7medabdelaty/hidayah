9e0a1a718f175bf69fe611ce9f5dd4a3b6a0b55a
A7medabdelaty <ahmed.abdelaty174@gmail.com>
2025-04-25 01:41:11 +0300

    test(widget_test): enhance app smoke test with Bloc and service locator
    Add MultiBlocProvider to include LocaleCubit and ThemeCubit in the widget tree. Also, initialize the service locator in the setUp method to ensure proper test setup. These changes improve the test's coverage and reliability by simulating the actual app environment more accurately.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,22 +1,38 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hidayah/app.dart';
+import 'package:hidayah/core/constants/app_constants.dart';
+import 'package:hidayah/core/cubit/locale/locale_cubit.dart';
+import 'package:hidayah/core/cubit/theme/theme_cubit.dart';
 import 'package:hidayah/core/routing/app_router.dart';
+import 'package:hidayah/core/services/service_locator.dart';
 
 void main() {
+  setUp(() {
+    setupServiceLocator();
+  });
+
   testWidgets('App smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(
       EasyLocalization(
-        supportedLocales: const [Locale('en')],
+        supportedLocales: AppConstants.supportedLocales,
         path: 'assets/lang',
         fallbackLocale: const Locale('en'),
-        child: HidayahApp(
-          appRouter: AppRouter(),
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => LocaleCubit()),
+            BlocProvider(create: (_) => ThemeCubit()),
+          ],
+          child: HidayahApp(
+            appRouter: AppRouter(),
+          ),
         ),
       ),
     );
 
+    await tester.pumpAndSettle();
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
